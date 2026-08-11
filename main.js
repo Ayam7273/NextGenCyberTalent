646ead4b379f0345c60f20904f0ddc7aa684531e
afeezalawonde <afeezalawonde@gmail.com>
2026-08-11 18:04:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createUuid() {
 
   const bytes = Array.from({ length: 16 }, () => Math.floor(Math.random() * 256));
   bytes[6] = (bytes[6] & 0x0f) | 0x40;
-  bytes[8] = (bytes[8] & 0x3f) | 0x80;f
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
   return bytes.map((byte, index) => {
     const hex = byte.toString(16).padStart(2, '0');
